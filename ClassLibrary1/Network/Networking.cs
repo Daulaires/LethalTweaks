ed1966e8ad206e06d4a99f098b8233e017120bfd
Daulaires <102845355+Daulaires@users.noreply.github.com>
2024-04-17 22:03:20 -0500

    Added to proper files

---
+++ ClassLibrary1/Network/Networking.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Unity.Netcode;
+
+namespace NetworkingPatch.Network
+{
+    internal class Networking : NetworkBehaviour
+    {
+        public override void OnNetworkSpawn()
+        {
+            LevelEvent = null;
+
+            if (NetworkManager.Singleton.IsHost || NetworkManager.Singleton.IsServer)
+            {
+                Instance?.gameObject.GetComponent<NetworkObject>().Despawn();
+            }
+            Instance = this;
+
+            base.OnNetworkSpawn();
+        }
+
+        [ClientRpc]
+        public void EventClientRpc(string eventName)
+        {
+            LevelEvent?.Invoke(eventName);
+        }
+
+        public static event Action<String> LevelEvent;
+
+        public static Networking Instance { get; private set; }
+    }
+}
---
+++ ClassLibrary1/Patches/shovelPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+
+namespace Lethal_Company_Tweaks.Patches
+{
+    
+    [HarmonyPatch(typeof(Shovel))]
+    internal class ShovelPatch : GrabbableObject
+    {
+        private static int shovelHitForce;
+
+        public static int ShovelHitForce { get => shovelHitForce; set => shovelHitForce = value; }
+
+        [HarmonyPatch("HitShovel")]
+        [HarmonyPostfix]
+        public static void OnInteract()
+        {
+            
+            // now we assign the new value to the shovelHitForce
+            shovelHitForce = 2;
+
+        }
+    }
+}
